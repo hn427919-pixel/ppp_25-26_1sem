595d80d73badb47446dfff51dffab3ed55e7a6b4
hn427919-pixel <hn427919@gmail.com>
2025-10-24 09:54:50 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,25 @@
 
+import random
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+n = int(input())
+m = int(input())
+
+s = [[random.randint(1, 100) for _ in range(m)] for _ in range(n)]
+
+print(s)
+for t in s:
+    print(t)
+
+strok = [max(t) for t in s]
+print(strok)
+
+stolb = [max(s[i][j] for i in range(n)) for j in range(m)]
+print(stolb)
+
+dig = sum(s[i][i] for i in range(min(n, m)))
+print(dig)
+
+sums = [sum(t) for t in s]
+r = max(sums)
+k = sums.index(r)
+print(k, r)
