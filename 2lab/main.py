9276e577acc288e8ea9c75090191264e244a7ce3
hn427919-pixel <hn427919@gmail.com>
2025-10-24 09:57:01 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,19 @@
+i,z=map(str,input("").split())
+p=[]
+o=[]
+y=[]
+u=[]
+for x in 'qwertyuiopasdfghjklzxcvbnm':
+    if x in i:
+        p.append(x)
+        o.append(i.count(x))
+for x in 'qwertyuiopasdfghjklzxcvbnm':
+    if x in z:
+        y.append(x)
+        u.append(z.count(x))
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+for x in range(len(o)):
+    for e in range(len(o)):
+        if o[x]==u[e]:
+             print(f'{p[x]}={y[e]}')
